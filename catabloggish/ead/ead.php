962a17b98d2c8f856a1445a74fafe62292d30b58
rubinsztajn <rubinsztajn@40485f78-fc63-11de-88c4-f1fccdb10200>
2010-01-14 17:12:15 +0000

    Added check for xsl extension and throw user-friendly error if not present.  Also added some more comments.

--- catabloggish/ead/ead.php
+++ catabloggish/ead/ead.php
@@ -7,37 +7,37 @@ Import EAD files, save them to the uploads folder, and insert html content into 
 add_action('admin_menu', 'ead_menu');
 
 
-// Retrieve and process options.
+/* Retrieve and process options */
 function ead_menu() {
     if ( $_GET['page'] ==  'ead') {
-	  if ( 'save' == $_REQUEST['action'] ) {
-	    if ($_FILES) {
-	      $f = ead_upload_file($_FILES);
-	      if ( $f['error'] ) {
-		update_option( 'ead-error', $f['error'] );
-		header("Location: edit.php?page=ead.php&error=true");
-		die;
-	      } else {
-	         update_option( 'ead-title', $_REQUEST['ead-title' ] );
-		 update_option( 'ead-file', $f['file' ] );
-		 header("Location: edit.php?page=ead.php&saved=true");
-		 die;
-	      }
-	    }
+      if ( 'save' == $_REQUEST['action'] ) {
+	if ($_FILES) {
+	  $f = ead_upload_file($_FILES);
+	  if ( $f['error'] ) {
+	    update_option( 'ead-error', $f['error'] );
+	    header("Location: edit.php?page=ead.php&error=true");
+	    die;
+	  } else {
+	    update_option( 'ead-title', $_REQUEST['ead-title' ] );
+	    update_option( 'ead-file', $f['file' ] );
+	    header("Location: edit.php?page=ead.php&saved=true");
+	    die;
 	  }
-  } 
-  add_posts_page('Import EAD', 'Import EAD', 'manage_options', 'ead', 'ead_options');
+	}
+      }
+    } 
+    add_posts_page('Import EAD', 'Import EAD', 'manage_options', 'ead', 'ead_options');
 }
 
 add_filter('upload_mimes', 'ead_xml_mime');
 
-// Add text/xml mime type to list of allowable uploads
+/* Add text/xml mime type to list of allowable uploads */
 function ead_xml_mime() {
   $mimes['xml'] = 'text/xml';
   return $mimes;
 }
 
-// Upload file
+/* Upload EAD file to uploads folder */
 function ead_upload_file($fileinfo) {
   if ($fileinfo['ead-file']['name'] != '') {
     $file = file_get_contents($fileinfo['ead-file']['tmp_name']);
@@ -46,17 +46,27 @@ function ead_upload_file($fileinfo) {
   }
 }
 
-// Layout options page
+/* Layout options page */
 function ead_options() {
   
+  // Check for PHP XSL extension
+  if (!extension_loaded('xsl')) {
+    echo '<div id="message" class="error"><p><strong>In order to use the EAD Importer, the PHP extension XSL must be enabled.  Go here for <a href="http://code.google.com/p/catabloggish">more information</a></strong></p></div>'; 
+    die;
+
+  }
+  
+  // Check for errors 
   if ( $_REQUEST['error'] ) {
     echo '<div id="message" class="error"><p><strong>' . get_option('ead-error') . '</strong></p></div>';
+    // If none, create post with transformation result  
   } elseif ( $_REQUEST['saved'] ) {
     $id = ead_create_post();
     if ( $id ) {
       $view_url = get_bloginfo('url') . '/?p=' . $id;
       $edit_url = get_bloginfo('url') . '/wp-admin/post.php?action=edit&post=' . $id;
       echo '<div id="message" class="updated fade"><p><strong>Collection posted&nbsp;  <a href="' . $view_url . '">View</a> :: <a href="' . $edit_url . '">Edit</a></strong></p></div>';
+      //If we don't get a post id, something went wrong
     } else {
       echo '<div id="message" class="updated fade"><p><strong>Oops... Something went wrong when creating the post</strong></p></div>';
     }
@@ -99,13 +109,14 @@ function ead_options() {
 
 
 
-// Create post from xslt output
+/* Create post from xslt output */
 function ead_create_post() {
 
+  // Collect options
   $filepath = get_option('ead-file');
   $title = get_option('ead-title');
 
-  // Post object
+  // Create post object
   $my_post = array();
   $my_post['post_title'] = $title;
   $my_post['post_content'] = ead_transform($filepath) ;
@@ -117,13 +128,14 @@ function ead_create_post() {
 }
 
 
-// Transform EAD/XML to WordPress post HTML
+/* Transform EAD/XML to HTML for a WordPress post */
 function ead_transform($file) {
 
   $xml_file = simplexml_load_file($file);
   $ead = $xml_file->asXML();
   $xslt = new XSLTProcessor();
   $xsl = new DOMDocument;
+  //Set path for xsl stylesheet
   $xslt_path = TEMPLATEPATH . '/ead/xslt/catablog_ead.xsl';
   $xsl->load($xslt_path);
   $xslt->importStyleSheet($xsl);
